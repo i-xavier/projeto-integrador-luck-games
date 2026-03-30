75276d055e62a709e645e06951bd1682f20decdb
PIETRO FERNANDO RODRIGUES DE OLIVEIRA <pietro.froliveira@senacsp.edu.br>
2026-03-30 15:25:26 -0300

    a

--- projeto-integrador/FormHome.Designer.cs
+++ projeto-integrador/FormHome.Designer.cs
@@ -37,12 +37,15 @@
             this.btnOrdens = new System.Windows.Forms.Button();
             this.btnClientes = new System.Windows.Forms.Button();
             this.btnDashboard = new System.Windows.Forms.Button();
-            this.panelLogo = new System.Windows.Forms.Panel();
-            this.pbLogo = new System.Windows.Forms.PictureBox();
             this.panelFundo = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.panelSideBar.SuspendLayout();
+            this.panelFundo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panelLogo.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // panelSideBar
@@ -70,10 +73,10 @@
             this.btnAcompanhamento.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAcompanhamento.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAcompanhamento.ForeColor = System.Drawing.Color.White;
-            this.btnAcompanhamento.Location = new System.Drawing.Point(0, 370);
+            this.btnAcompanhamento.Location = new System.Drawing.Point(0, 382);
             this.btnAcompanhamento.Name = "btnAcompanhamento";
             this.btnAcompanhamento.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.btnAcompanhamento.Size = new System.Drawing.Size(236, 45);
+            this.btnAcompanhamento.Size = new System.Drawing.Size(236, 47);
             this.btnAcompanhamento.TabIndex = 7;
             this.btnAcompanhamento.Text = "          Acompanhamento";
             this.btnAcompanhamento.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -87,10 +90,10 @@
             this.btnAparelhos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAparelhos.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAparelhos.ForeColor = System.Drawing.Color.White;
-            this.btnAparelhos.Location = new System.Drawing.Point(0, 325);
+            this.btnAparelhos.Location = new System.Drawing.Point(0, 335);
             this.btnAparelhos.Name = "btnAparelhos";
             this.btnAparelhos.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.btnAparelhos.Size = new System.Drawing.Size(236, 45);
+            this.btnAparelhos.Size = new System.Drawing.Size(236, 47);
             this.btnAparelhos.TabIndex = 6;
             this.btnAparelhos.Text = "          Aparelhos";
             this.btnAparelhos.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -104,10 +107,10 @@
             this.btnEstoque.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnEstoque.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnEstoque.ForeColor = System.Drawing.Color.White;
-            this.btnEstoque.Location = new System.Drawing.Point(0, 280);
+            this.btnEstoque.Location = new System.Drawing.Point(0, 288);
             this.btnEstoque.Name = "btnEstoque";
             this.btnEstoque.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.btnEstoque.Size = new System.Drawing.Size(236, 45);
+            this.btnEstoque.Size = new System.Drawing.Size(236, 47);
             this.btnEstoque.TabIndex = 5;
             this.btnEstoque.Text = "          Estoque";
             this.btnEstoque.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -121,10 +124,10 @@
             this.btnVendas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnVendas.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnVendas.ForeColor = System.Drawing.Color.White;
-            this.btnVendas.Location = new System.Drawing.Point(0, 235);
+            this.btnVendas.Location = new System.Drawing.Point(0, 241);
             this.btnVendas.Name = "btnVendas";
             this.btnVendas.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.btnVendas.Size = new System.Drawing.Size(236, 45);
+            this.btnVendas.Size = new System.Drawing.Size(236, 47);
             this.btnVendas.TabIndex = 4;
             this.btnVendas.Text = "          Vendas";
             this.btnVendas.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -138,10 +141,10 @@
             this.btnOrdens.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnOrdens.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnOrdens.ForeColor = System.Drawing.Color.White;
-            this.btnOrdens.Location = new System.Drawing.Point(0, 190);
+            this.btnOrdens.Location = new System.Drawing.Point(0, 194);
             this.btnOrdens.Name = "btnOrdens";
             this.btnOrdens.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.btnOrdens.Size = new System.Drawing.Size(236, 45);
+            this.btnOrdens.Size = new System.Drawing.Size(236, 47);
             this.btnOrdens.TabIndex = 3;
             this.btnOrdens.Text = "          Ordens de Serviço";
             this.btnOrdens.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -157,10 +160,10 @@
             this.btnClientes.ForeColor = System.Drawing.Color.White;
             this.btnClientes.Image = ((System.Drawing.Image)(resources.GetObject("btnClientes.Image")));
             this.btnClientes.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnClientes.Location = new System.Drawing.Point(0, 145);
+            this.btnClientes.Location = new System.Drawing.Point(0, 147);
             this.btnClientes.Name = "btnClientes";
             this.btnClientes.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.btnClientes.Size = new System.Drawing.Size(236, 45);
+            this.btnClientes.Size = new System.Drawing.Size(236, 47);
             this.btnClientes.TabIndex = 2;
             this.btnClientes.Text = "          Clientes";
             this.btnClientes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -179,17 +182,40 @@
             this.btnDashboard.Location = new System.Drawing.Point(0, 100);
             this.btnDashboard.Name = "btnDashboard";
             this.btnDashboard.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.btnDashboard.Size = new System.Drawing.Size(236, 45);
+            this.btnDashboard.Size = new System.Drawing.Size(236, 47);
             this.btnDashboard.TabIndex = 1;
             this.btnDashboard.Text = "          Dashboard";
             this.btnDashboard.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnDashboard.UseVisualStyleBackColor = true;
             this.btnDashboard.Click += new System.EventHandler(this.btnDashboard_Click);
             // 
+            // panelFundo
+            // 
+            this.panelFundo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelFundo.BackColor = System.Drawing.Color.Black;
+            this.panelFundo.Controls.Add(this.pictureBox1);
+            this.panelFundo.Location = new System.Drawing.Point(0, 0);
+            this.panelFundo.Name = "panelFundo";
+            this.panelFundo.Size = new System.Drawing.Size(934, 561);
+            this.panelFundo.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(400, 145);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(372, 270);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
             // panelLogo
             // 
-            this.panelLogo.BackColor = System.Drawing.Color.White;
-            this.panelLogo.Controls.Add(this.pbLogo);
+            this.panelLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(52)))), ((int)(((byte)(68)))));
+            this.panelLogo.Controls.Add(this.pictureBox2);
             this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelLogo.ForeColor = System.Drawing.SystemColors.ControlText;
             this.panelLogo.Location = new System.Drawing.Point(0, 0);
@@ -197,25 +223,16 @@
             this.panelLogo.Size = new System.Drawing.Size(236, 100);
             this.panelLogo.TabIndex = 0;
             // 
-            // pbLogo
+            // pictureBox2
             // 
-            this.pbLogo.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pbLogo.Image = ((System.Drawing.Image)(resources.GetObject("pbLogo.Image")));
-            this.pbLogo.Location = new System.Drawing.Point(0, 0);
-            this.pbLogo.Name = "pbLogo";
-            this.pbLogo.Size = new System.Drawing.Size(236, 100);
-            this.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pbLogo.TabIndex = 0;
-            this.pbLogo.TabStop = false;
-            // 
-            // panelFundo
-            // 
-            this.panelFundo.BackColor = System.Drawing.Color.Silver;
-            this.panelFundo.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelFundo.Location = new System.Drawing.Point(0, 0);
-            this.panelFundo.Name = "panelFundo";
-            this.panelFundo.Size = new System.Drawing.Size(934, 561);
-            this.panelFundo.TabIndex = 1;
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(-3, -20);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(236, 148);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
             // 
             // FormHome
             // 
@@ -230,8 +247,10 @@
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.FormHome_Load);
             this.panelSideBar.ResumeLayout(false);
+            this.panelFundo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panelLogo.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -240,7 +259,6 @@
 
         private System.Windows.Forms.Panel panelSideBar;
         private System.Windows.Forms.Panel panelFundo;
-        private System.Windows.Forms.Panel panelLogo;
         private System.Windows.Forms.Button btnDashboard;
         private System.Windows.Forms.Button btnAcompanhamento;
         private System.Windows.Forms.Button btnAparelhos;
@@ -248,6 +266,8 @@
         private System.Windows.Forms.Button btnVendas;
         private System.Windows.Forms.Button btnOrdens;
         private System.Windows.Forms.Button btnClientes;
-        private System.Windows.Forms.PictureBox pbLogo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
